4b1ac3dc7e6da7e9a9156f5a2e3cce5ba93f5bd3
Ali Waseem <waseema393@gmail.com>
2015-02-18 23:34:59 -0500

    Finished first draft of the first home page and added multiple modules to help with review site

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -115,6 +115,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'django_project/static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATIC_URL = '/static/'
--- django_project/home/views.py
+++ django_project/home/views.py
@@ -5,4 +5,4 @@ from home.models import Header
 def index(request):
     current_header = get_object_or_404(Header, current_header=True)
     context = {"current_header": current_header}
-    return render(request, "home/index.html", context)
+    return render(request, "home/index_test.html", context)
