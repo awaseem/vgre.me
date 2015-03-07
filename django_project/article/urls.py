18cb84d84efc3f0d77ccf95629e9a98dc5158f0d
Ali Waseem <waseema393@gmail.com>
2015-03-07 00:47:37 -0500

    finished search template and filtering for search bar

--- django_project/article/urls.py
+++ django_project/article/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns("",
 
     url(r"^game/(?P<game_id>[\w ]+)/$", views.article, name="article"),
     url(r"^search/$", views.search, name="reviews"),
-    url(r"^search/results/(?P<search_query>[\w ]+)/$", views.results, name="results"),
+    url(r"^search/results/(?P<search_query>(.*))/$", views.results, name="results"),
 
 )
--- django_project/article/views.py
+++ django_project/article/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, get_list_or_404 ,redirect
 from article.models import Article
+from home.models import Header
 from watson import search as watsonSearch
 
 
@@ -13,16 +14,28 @@ def article(request, game_id):
 
 def search(request):
     if request.method == "POST":
-        search_query = request.POST.get("search_query", None)
-        return redirect("results", search_query)
-    else:
-        return render(request, "article/reviews.html")
+        search_query = request.POST.get("search_query")
+        if search_query:
+            return redirect("results", search_query)
+    return render(request, "article/reviews.html", getSearchContext())
 
 
 def results(request, search_query):
-    print search_query
+    context = getSearchContext()
     search_results = watsonSearch(search_query)
-    context = {
-        "results": search_results
-    }
+    if search_results:
+        context["results"] = search_results
+    else:
+        context["err_message"] = "Sorry! No Game Reviews Found :("
     return render(request, "article/reviews.html", context)
+
+
+def getSearchContext():
+    # Obtain search context for the search page, this logic is used twice. Hence its wrapped in a function
+    current_header = get_object_or_404(Header, current_header=True)
+    current_articles = get_list_or_404(Article.objects.order_by("-published_date"), published_status=True)[:9]
+    context = {
+        "current_header": current_header,
+        "current_articles": current_articles,
+    }
+    return context
