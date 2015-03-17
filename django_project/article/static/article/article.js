0377c26f27f35669df9a95a65226efcec9f971e9
Ali Waseem <waseema393@gmail.com>
2015-03-16 22:51:43 -0400

    Added scroll to top button and created fade in based on window scrolling

--- django_project/article/static/article/article.js
+++ django_project/article/static/article/article.js
@@ -4,7 +4,6 @@
 
 $(document).ready(function() {
     $("#header-text, #header-posted, #header-subtext, #menu-toggle").css({opacity: 0});
-    $(".latest-reviews").css({opacity: 0});
     $("header, footer").css({opacity: 0}).delay(500).fadeTo(1000, 1, function() {
         $("#menu-toggle").fadeTo(1000, 1);
         $("#header-text").fadeTo(500, 1, function () {
@@ -12,6 +11,21 @@ $(document).ready(function() {
                 $("#header-posted").fadeTo(500, 1);
             });
         });
-        $(".latest-reviews").fadeTo(1000, 1);
     });
+
+  	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 350) {
+			$('.top').fadeIn();
+		} else {
+			$('.top').fadeOut();
+		}
+	});
+
+	//Click event to scroll to top
+	$('.top').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
 });
--- django_project/home/static/home/home.js
+++ django_project/home/static/home/home.js
@@ -3,6 +3,7 @@
  */
 
 $(document).ready(function() {
+
     $("#header-text, #header-bar, #header-subtext, #menu-toggle").css({opacity: 0});
     $(".latest-reviews").css({opacity: 0});
     $("header, footer").css({opacity: 0}).delay(500).fadeTo(1000, 1, function() {
@@ -11,6 +12,11 @@ $(document).ready(function() {
             $("#header-bar").fadeTo(500, 1);
             $("#header-subtext").delay(500).fadeTo(500, 1);
         });
-        $(".latest-reviews").fadeTo(1000, 1);
+    });
+
+    $(window).on("scroll", function() {
+        if ($(this).scrollTop() >= 200){
+            $(".latest-reviews").fadeTo(1000, 1);
+        }
     });
 });
